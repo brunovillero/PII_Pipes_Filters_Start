eb5106898b5b65430abe7192214a500eae98341c
matiasojeda13 <matias.ojeda@correo.ucu.edu.com>
2020-10-09 21:13:38 -0300

    Program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using CompAndDel.Filters;
+using CompAndDel.Pipes;
 
 namespace CompAndDel
 {
@@ -6,6 +8,13 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+            PictureProvider p = new PictureProvider();
+            IPicture pic = p.GetPicture("C:/Users/matia/c#/pipes-filter/PII_Pipes_Filters_Start/src/Adds/índice.jpg");
+            IFilter filter1 = new FilterGreyscale();
+            IPipe firstpipe = new PipeSerial(filter1, secondpipe);
+            IFilter filter2 = new FilterNegative();
+            IPipe secondpipe = new PipeSerial(filter2, );
+            IPipe thirdpipe = new PipeNull();
         }
     }
 }
