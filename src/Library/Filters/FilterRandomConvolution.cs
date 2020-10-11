28dd3e8ef84dd0738890e9075217ee9bd81b61a9
brunovillero <brunovillero66@gmail.com>
2020-10-11 20:54:16 -0300

    Herencia en convolution filter

--- src/Library/Filters/FilterRandomConvolution.cs
+++ src/Library/Filters/FilterRandomConvolution.cs
@@ -4,22 +4,23 @@ using CompAndDel;
 
 namespace CompAndDel.Filters
 {
-    public class FilterBlurConvolution : FilterConvolution
+    public class FilterRandomConvolution : FilterConvolution
     {
+        private Random random = new Random();
         /// <summary>
-        /// Filtro complejo que suaviza los bordes de una imagen.
+        /// Filtro complejo random.
         /// </summary>
         /// <param name="name">Nombre del objeto</param>
-        public FilterBlurConvolution()
+        public FilterRandomConvolution()
         {
             this.matrizParametros = new int[3, 3];
-            this.complemento = 0;
-            this.divisor = 9;
+            this.complemento = random.Next(1,7);     
+            this.divisor = random.Next(1,33); 
             for (int x = 0; x < 3; x++)
             {
                 for (int y = 0; y < 3; y++)
                 {
-                    matrizParametros[x, y] = 1;
+                    matrizParametros[x, y] = random.Next(1,5); 
                 }
             }
         }
